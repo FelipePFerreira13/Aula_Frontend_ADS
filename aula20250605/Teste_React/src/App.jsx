c8563379c36667fa7ad8e0ce54827e54e2cd9aaa
Felipe Pires Ferreira <felipepferreira13@gmail.com>
2025-06-12 19:53:52 -0300

    Update_Last_Week

---
+++ aula20250605/Teste_React/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App(){
+  return <div>
+    <TitleColorido/>
+    <TitleColorido/>
+    </div>
+}
+function TitleColorido(){
+  return <h1 className='titulo_colorido'>Titulo colorido</h1>
+}
+export default App
